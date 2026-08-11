3c78762fb376b7754cf45e8af3cac4260753793d
Aleksandra <alek.schepis09@gmail.com>
2026-08-11 13:51:05 -0300

    cardápip pwa

---
+++ script.js
@@ -0,0 +1,38 @@
+const cartCount = document.querySelector("#cart-count");
+const addButtons = document.querySelectorAll(".add-button");
+
+let totalItems = 0;
+
+addButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    totalItems++;
+    cartCount.textContent = totalItems;
+
+    // Feedback visual temporário no próprio botão.
+    const originalText = button.textContent;
+
+    button.textContent = "Adicionado!";
+    button.disabled = true;
+
+    setTimeout(() => {
+      button.textContent = originalText;
+      button.disabled = false;
+    }, 700);
+  });
+});
+
+
+const installButton = document.getElementById("install-button");
+
+let promptEvent
+
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    promptEvent = event;
+
+});
+
+installButton.addEventListener("click", () => {
+    if (!promptEvent) return;
+    promptEvent.prompt();
+});
