f0113d7ca354c8e263924faea06679ea32b521b5
Aleksandra <alek.schepis09@gmail.com>
2026-08-11 14:36:12 -0300

    fix no js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-const cartCount = document.querySelector("#cart-count");
-const addButtons = document.querySelectorAll(".add-button");
+// const cartCount = document.querySelector("#cart-count");
+// const addButtons = document.querySelectorAll(".add-button");
 
-let totalItems = 0;
+// let totalItems = 0;
 
-addButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    totalItems++;
-    cartCount.textContent = totalItems;
+// addButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     totalItems++;
+//     cartCount.textContent = totalItems;
 
-    // Feedback visual temporário no próprio botão.
-    const originalText = button.textContent;
+//     // Feedback visual temporário no próprio botão.
+//     const originalText = button.textContent;
 
-    button.textContent = "Adicionado!";
-    button.disabled = true;
+//     button.textContent = "Adicionado!";
+//     button.disabled = true;
 
-    setTimeout(() => {
-      button.textContent = originalText;
-      button.disabled = false;
-    }, 700);
-  });
-});
+//     setTimeout(() => {
+//       button.textContent = originalText;
+//       button.disabled = false;
+//     }, 700);
+//   });
+// });
 
 
-const installButton = document.getElementById("install-button");
+// const installButton = document.getElementById("install-button");
 
-let promptEvent
+// let promptEvent
 
-window.addEventListener("beforeinstallprompt", (event) => {
-    event.preventDefault();
-    promptEvent = event;
+// window.addEventListener("beforeinstallprompt", (event) => {
+//     event.preventDefault();
+//     promptEvent = event;
 
-});
+// });
 
-installButton.addEventListener("click", () => {
-    if (!promptEvent) return;
-    promptEvent.prompt();
-});
+// installButton.addEventListener("click", () => {
+//     if (!promptEvent) return;
+//     promptEvent.prompt();
+// });
